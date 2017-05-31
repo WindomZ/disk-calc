ea05852e9f5b8e1d8f943eab23ce4eba50720a92
WindomZ <git.windomz@gmail.com>
2017-05-31 08:56:55 +0800

    usage command impl

--- bin/disk-calc.js
+++ bin/disk-calc.js
@@ -22,10 +22,32 @@ program
 program
   .command('usage')
   .alias('u')
+  .option('-c, --cn', 'chinese', undefined, undefined)
   .action(options => {
     noArgs = false
 
-    console.log('Function is still under development...')
+    if (options.cn) {
+      process.stdout.write('1 >>> '.yellow + '当Windows位于第一个区域时，' +
+        '它会自动创建一个额外的三个隐藏的分区。' + os.EOL)
+      process.stdout.write('2 >>> '.yellow + '所以当计算第一个分区时，' +
+        '应该首先删除所有隐藏的分区。' + os.EOL)
+      process.stdout.write('3 >>> '.yellow + '尝试创建一个分区，' +
+        '获取所有隐藏分区的总大小。' + os.EOL)
+      process.stdout.write('4 >>> '.yellow + '再次删除全部重新创建，' +
+        '然后C盘分区大小携带隐藏分区大小的总和。' + os.EOL)
+      process.stdout.write('5 >>> '.yellow + '其他分区大小不需要携带隐藏分区大小的总和。' + os.EOL)
+    } else {
+      process.stdout.write('1 >>> '.yellow + 'When Windows is in the first area, ' +
+        'it automatically creates an additional three hidden partitions.' + os.EOL)
+      process.stdout.write('2 >>> '.yellow + 'So when calculating the first partition, ' +
+        'you should first remove all hidden partitions.' + os.EOL)
+      process.stdout.write('3 >>> '.yellow + 'Try to create a partition, ' +
+        'get the total size of all hidden partitions.' + os.EOL)
+      process.stdout.write('4 >>> '.yellow + 'Again delete all re-create, ' +
+        'and then C drive partition size to carry the sum of the hidden partition size.' + os.EOL)
+      process.stdout.write('5 >>> '.yellow + 'Other partition sizes ' +
+        'do not need to carry the sum of the hidden partition sizes.' + os.EOL)
+    }
   })
 
 program
